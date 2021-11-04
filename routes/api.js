2fa8adb71d84c5605e05b22bc9d357d92f7bafbb
lvaillan <classvaillan@gmail.com>
2021-11-04 15:51:05 -0500

    undo route changes

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,6 @@
-const path = require('path');
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-//index
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
 
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
